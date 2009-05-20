f81c7446aa281b43545a9f633998acb361e12756
Dan Sheppard <dan.sheppard@caret.cam.ac.uk>
2009-05-20 20:03:24 +0000

    CSPACE-172 Partial refactoring of controller.

--- chain/src/main/java/org/collectionspace/HandleJSON.java
+++ chain/src/main/java/org/collectionspace/HandleJSON.java
@@ -1,20 +1,14 @@
 package org.collectionspace;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Iterator;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -26,94 +20,64 @@ public class HandleJSON extends HttpServlet
 	private final static String STORE_REF = "/store/object/";
 
 	final static String ABSOLUTE_PATH = "/";
-	
+
 	JSONStore store=new StubJSONStore();
-	
+
 	/**
 	 * Responding to a request. The request is assumed to consist of a path to a requested JSON object.
 	 * The response returns the object in string form (or an empty string if not found).
 	 */
 	@Override
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	public void doGet(HttpServletRequest servlet_request, HttpServletResponse servlet_response) throws ServletException, IOException
 	{
-		
+
 		// The input should just be a string identifying the path to the file
-		String path = req.getPathInfo();
-		if (path == null || path.length() == 0 || 	path.indexOf(SCHEMA_REF) < 0)
+		String path = servlet_request.getPathInfo();
+		if (path == null || path.length() == 0 )
 		{
-			notifyUsage(resp);
+			notifyUsage(servlet_response);
 			return;
 		}
 
 		// Set response type to JSON
-		resp.setCharacterEncoding("UTF-8");
-		resp.setContentType("application/json");
+		servlet_response.setCharacterEncoding("UTF-8");
+		servlet_response.setContentType("application/json");
 
 		String outputJSON;
+
+
 		// Get the local path, either from the SCHEMA_REF or from the STORE_REF, depending on which one
 		// is present
-		int pos = path.indexOf(SCHEMA_REF); 
-		if (pos >= 0)
-		{
-			int from = pos + SCHEMA_REF.length();
-			if (from < path.length())
-			{
-				path = path.substring(from);
-			}
-			else
-			{
-				resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "No file path supplied after " + SCHEMA_REF);
-				return;
-			}
-		}
-		else
-		{
-			pos = path.indexOf(STORE_REF);
-			if (pos >= 0)
-			{
-				// get the path after store (should be the file path not including leading /)
-				int from = pos + STORE_REF.length();
-				if (from < path.length())
-				{
-					path = path.substring(from);
-				}
-				else
-				{
-					resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "No file path supplied after " + STORE_REF);
-					return;
-				}
-			}
-		}
-
-		try
-		{
+		try {
+			ChainRequest request=new ChainRequest(servlet_request,servlet_response);
+			path=request.getPathTail();
 			outputJSON = store.retrieveJson(ABSOLUTE_PATH + path);
-		}
-		catch (NotFoundException nfe)
-		{
-			resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "File not found: " + path);
-			return;
+
+			// If there is no output something must have gone wrong
+			if (outputJSON == null || outputJSON.length() == 0)
+			{
+				servlet_response.sendError(HttpServletResponse.SC_BAD_REQUEST, "No JSON found");
+				return;			
+			}
+
+			// Write the requested JSON out
+			PrintWriter out = servlet_response.getWriter();
+			try
+			{
+				out.write(outputJSON);
+				out.close();
+				servlet_response.setStatus(HttpServletResponse.SC_OK);
+			}
+			catch (Exception onfe)
+			{
+				servlet_response.sendError(HttpServletResponse.SC_NOT_FOUND, "Data " + path + " not found");
+			}
+
+		} catch (BadRequestException x) {
+			servlet_response.sendError(HttpServletResponse.SC_BAD_REQUEST, x.getMessage());
 		}
 
-		// If there is no output something must have gone wrong
-		if (outputJSON == null || outputJSON.length() == 0)
-		{
-			resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "No JSON found");
-			return;			
-		}
 
-		// Write the requested JSON out
-		PrintWriter out = resp.getWriter();
-		try
-		{
-			out.write(outputJSON);
-			out.close();
-			resp.setStatus(HttpServletResponse.SC_OK);
-		}
-		catch (Exception onfe)
-		{
-			resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Data " + path + " not found");
-		}
 	}
 
 	@Override
@@ -197,9 +161,9 @@ public class HandleJSON extends HttpServlet
 		resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "You must structure the requests like so: \n" +
 				"GET /schema/%path-to-file-with-name% \n" +
 				"GET /store/object/%path-to-file-with-name% \n" +
-				"POST /store/object/%path-to-file-with-name% - note that data in body must be JSON \n");
+		"POST /store/object/%path-to-file-with-name% - note that data in body must be JSON \n");
 	}
-	
+
 	public static class NotFoundException extends RuntimeException {
 		public NotFoundException(String message, Throwable cause) {
 			super(message, cause);
@@ -210,4 +174,3 @@ public class HandleJSON extends HttpServlet
 	}
 
 }
-
