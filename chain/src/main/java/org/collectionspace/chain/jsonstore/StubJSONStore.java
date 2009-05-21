95b2e81304ce3e7ec391ef804ab974d7af1a13bb
Dan Sheppard <dan.sheppard@caret.cam.ac.uk>
2009-05-21 21:14:27 +0000

    CSPACE-172 Remove JSON parsing and serialisation. Sorry, this was my mistake in the spec. We don't need to parse our own format, just go to+from JSON to ensure validity. That's not how my spec reads at all, which means it was far more complicated a problem than it need be. Now reverted to simpler form because it makes the on-disk format simpler.

--- chain/src/main/java/org/collectionspace/chain/jsonstore/StubJSONStore.java
+++ chain/src/main/java/org/collectionspace/chain/jsonstore/StubJSONStore.java
@@ -1,101 +1,34 @@
 package org.collectionspace.chain.jsonstore;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Iterator;
 
-import org.json.JSONArray;
+import org.apache.commons.io.IOUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class StubJSONStore implements JSONStore {
-	private final static String JSON_TYPE_STRING = "string";
-	private final static String JSON_TYPE_BOOLEAN = "boolean";
-	private final static String JSON_TYPE_NUMBER = "number";
-	private final static String JSON_TYPE_JSONObject = "object";
-	private final static String JSON_TYPE_JSONArray = "array";
-	private final static String JSON_TYPE_NULL = "null";
-		
+/* We should probably store JSON as JSON. We'll still parse and serialise it, though.
+ *  Sorry, that was my mistake, Avi, :(. -- dan
+ */
+
+public class StubJSONStore implements JSONStore {		
 	/* (non-Javadoc)
 	 * @see org.collectionspace.JSONStore#retrieveJson(java.lang.String)
 	 */
 	public String retrieveJson(String filePath) throws JSONNotFoundException {
-		JSONObject jsonObject = new JSONObject();
 		File jsonFile = new File(filePath);
 		if (!jsonFile.exists())
 		{
 			throw new JSONNotFoundException("No such file: " + filePath);
 		}
 
-		// Put together a JSON object form the file. The file is assumed to contain one entry per line, where
-		// an entry consists of the triplet type-key-value, separated by spaces.
 		try {
-			BufferedReader reader = new BufferedReader(new FileReader(jsonFile));
-			String line = reader.readLine();
-			while (line != null)
-			{
-				String type = "";
-				String key = "";
-				String value = "";
-
-				// Ignore lines that do not follow the precise "type key value" format
-				// First parameter is type
-				int pos = line.indexOf(" ");
-				if (pos > 0)
-				{
-					type = line.substring(0,pos).trim();
-					line = line.substring(pos).trim();
-					// Second parameter is key
-					pos = line.indexOf(" ");
-					if (pos > 0)
-					{
-						key = line.substring(0,pos).trim();
-						// Third parameter is value, made up of the rest of the line
-						value = line.substring(pos).trim();
-					}
-					else
-					{
-						return "";
-					}
-				}
-				else
-				{
-					return "";
-				}
-
-				if (type.equals(JSON_TYPE_BOOLEAN))
-				{
-					jsonObject.put(key, new Boolean(value));
-				}
-				// Numbers are put in as Doubles
-				else if (type.equals(JSON_TYPE_NUMBER))
-				{
-					jsonObject.put(key, new Double(value));
-				}
-				else if (type.equals(JSON_TYPE_STRING))
-				{
-					jsonObject.put(key, value);
-				}
-				else if (type.equals(JSON_TYPE_JSONArray))
-				{
-					jsonObject.put(key, new JSONArray(value));
-				}
-				else if (type.equals(JSON_TYPE_JSONObject))
-				{
-					jsonObject.put(key, new JSONObject(value));	
-				}
-				else if (type.equals(JSON_TYPE_NULL))
-				{
-					jsonObject.put(key, JSONObject.NULL);
-				}
-
-				// Get the next line from the file
-				line = reader.readLine();
-			}
+			FileReader r=new FileReader(jsonFile);
+			String data=IOUtils.toString(r);
+			r.close();
+			JSONObject jsonObject = new JSONObject(data);
 			return jsonObject.toString();
 		}
 		catch (IOException ioe)
@@ -111,7 +44,6 @@ public class StubJSONStore implements JSONStore {
 	/* (non-Javadoc)
 	 * @see org.collectionspace.JSONStore#storeJson(java.lang.String, org.json.JSONObject)
 	 */
-	@SuppressWarnings("unchecked")
 	public void storeJson(String filePath, JSONObject jsonObject)
 	{
 		System.out.println("file path:" + filePath);
@@ -120,52 +52,9 @@ public class StubJSONStore implements JSONStore {
 		System.out.println("storing json");
 		try
 		{
-			BufferedWriter writer = new BufferedWriter(new FileWriter(jsonFile));
-
-			// Parse the JSON string into a file where each line is of the form "type key value".
-			for (Iterator iter = jsonObject.keys(); iter.hasNext(); )
-			{
-				String key = (String)iter.next();
-				Object value = jsonObject.get(key);
-				String type = "";
-				if (value == null)
-				{
-					type = JSON_TYPE_NULL;
-				}
-				else if (value instanceof Boolean)
-				{
-					type = JSON_TYPE_BOOLEAN;
-				}
-				else if (value instanceof Integer ||
-						value instanceof Float ||
-						value instanceof Double)
-				{
-					type = JSON_TYPE_NUMBER;
-				}
-				else if (value instanceof String)
-				{
-					type = JSON_TYPE_STRING;
-				}
-				else if (value instanceof JSONArray)
-				{
-					type = JSON_TYPE_JSONArray;
-				}
-				else if (value instanceof JSONObject)
-				{
-					type = JSON_TYPE_JSONObject;
-				}
-				// Only write out if there is a type
-				if (type != "")
-				{
-					writer.write(type + " " + key + " " + value + "\n");
-				}
-			}
-			writer.flush();
-			writer.close();
-		}
-		catch (JSONException je)
-		{
-			return;
+			FileWriter w=new FileWriter(jsonFile);
+			IOUtils.write(jsonObject.toString(),w);
+			w.close();
 		}
 		catch (IOException ioe)
 		{
