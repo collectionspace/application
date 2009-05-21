0e32253555678ff0c6d83d680b683b5ffb0baf15
Dan Sheppard <dan.sheppard@caret.cam.ac.uk>
2009-05-21 12:32:20 +0000

    CSPACE-172 Some refactoring of doPut. Mainly separating out controlelr concerns and using commons io utils, where appropriate.

--- src/main/java/org/collectionspace/chain/controller/ChainServlet.java
+++ src/main/java/org/collectionspace/chain/controller/ChainServlet.java
@@ -2,6 +2,7 @@ package org.collectionspace.chain.controller;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.IOUtils;
 import org.collectionspace.chain.jsonstore.JSONStore;
 import org.collectionspace.chain.jsonstore.StubJSONStore;
 import org.json.JSONException;
@@ -31,6 +33,15 @@ public class ChainServlet extends HttpServlet
 		return out;
 	}
 	
+	private boolean perhapsServeFixedContent(HttpServletRequest servlet_request, HttpServletResponse servlet_response) throws ServletException, IOException {
+		InputStream is=Thread.currentThread().getContextClassLoader().getResourceAsStream(servlet_request.getPathInfo());
+		if(is==null)
+			return false; // Not for us
+		// Serve fixed content
+		IOUtils.copy(is,servlet_response.getOutputStream());
+		return true;
+	}
+	
 	/**
 	 * Responding to a request. The request is assumed to consist of a path to a requested JSON object.
 	 * The response returns the object in string form (or an empty string if not found).
@@ -38,9 +49,11 @@ public class ChainServlet extends HttpServlet
 	@Override
 	public void doGet(HttpServletRequest servlet_request, HttpServletResponse servlet_response) throws ServletException, IOException
 	{
+		if(perhapsServeFixedContent(servlet_request,servlet_response))
+			return;
 		try {
 			// Setup our request object
-			ChainRequest request=new ChainRequest(servlet_request,servlet_response);
+			ChainRequest request=new ChainRequest(servlet_request,servlet_response,true);
 			
 			// Get the data
 			String outputJSON = getJSON(request.getPathTail());
@@ -67,52 +80,26 @@ public class ChainServlet extends HttpServlet
 	 * URL as the part following "/store/object/".
 	 */
 	@Override
-	public void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	public void doPut(HttpServletRequest servlet_request, HttpServletResponse servlet_response) throws ServletException, IOException
 	{
-		// The input should just be a string identifying the path to the file
-		String jsonString;
-		String path = req.getPathInfo();
-		if (path == null || path.length() == 0 || path.indexOf(STORE_REF) < 0)
-		{
-			// get the path and json from params
-			path = req.getParameter("storage");
-			jsonString = req.getParameter("json_str");
-		}
-		else
-		{
-			// get the json from the body
-			StringBuilder sb = new StringBuilder();
-			BufferedReader br = req.getReader();
-			String line = br.readLine();
-			while (line != null)
-			{
-				sb.append(line);
-				sb.append("\n");
-				line = br.readLine();
-			}
-			jsonString = sb.toString();
-		}
-
-		if (path != null && path.length() > 0 && path.indexOf(STORE_REF) >= 0)
-		{
-			int from = path.indexOf(STORE_REF) + STORE_REF.length();
-			if (path.length() > from)
-			{
-				path = path.substring(from);
-			}
-		}
-		else
-		{
-			notifyUsage(resp);
+		String path=null;
+		String jsonString=null;
+		// Setup our request object
+		try {
+			ChainRequest request=new ChainRequest(servlet_request,servlet_response,false);
+			path = request.getPathTail();
+			jsonString = request.getBody();
+		} catch (BadRequestException x) {
+			servlet_response.sendError(HttpServletResponse.SC_BAD_REQUEST, x.getMessage());			
 			return;
 		}
-
+		
 		// Set response type to text/html
-		resp.setContentType("text/html");
+		servlet_response.setContentType("text/html");
 
 		if (jsonString == null || jsonString.length() == 0)
 		{
-			resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "No JSON content to store");
+			servlet_response.sendError(HttpServletResponse.SC_BAD_REQUEST, "No JSON content to store");
 			return; // END
 		}
 		else
@@ -121,23 +108,15 @@ public class ChainServlet extends HttpServlet
 			try
 			{
 				store.storeJson(ABSOLUTE_PATH + path, new JSONObject(jsonString));
-				resp.addHeader("Location", STORE_REF + path);
-				resp.setStatus(201);
+				servlet_response.addHeader("Location", STORE_REF + path);
+				servlet_response.setStatus(201);
 				return; // END
 			}
 			catch (JSONException je)
 			{
-				resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Failed to parse json: " + je);
+				servlet_response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Failed to parse json: " + je);
 			}
 		}
-	}
-
-	private void notifyUsage(HttpServletResponse resp) throws IOException
-	{
-		resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "You must structure the requests like so: \n" +
-				"GET /schema/%path-to-file-with-name% \n" +
-				"GET /store/object/%path-to-file-with-name% \n" +
-		"POST /store/object/%path-to-file-with-name% - note that data in body must be JSON \n");
 	}
 
 	public static class NotFoundException extends RuntimeException {
