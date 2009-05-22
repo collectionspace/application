78ac072df299d7e88a29fb6e1f82add05e5f87e2
Dan Sheppard <dan.sheppard@caret.cam.ac.uk>
2009-05-22 16:13:13 +0000

    CSPACE-172 Converted unit tests to JUnit.

--- chain/src/main/test/org/collectionspace/HandleJSONTest.java
+++ chain/src/main/test/org/collectionspace/HandleJSONTest.java
@@ -1,11 +1,17 @@
 package org.collectionspace;
 
+import static org.junit.Assert.*;
+
+import java.io.File;
+
 import org.collectionspace.chain.jsonstore.ExistException;
 import org.collectionspace.chain.jsonstore.JSONNotFoundException;
 import org.collectionspace.chain.jsonstore.JSONStore;
 import org.collectionspace.chain.jsonstore.StubJSONStore;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
 
 public class HandleJSONTest {
 	
@@ -16,88 +22,42 @@ public class HandleJSONTest {
 	                        "\"thesis\"},{\"value\":\"2\",\"text\":\"paper\"},{\"value\":\"3\",\"text\":\"excel-controlled\"}]," +
 	                        "\"param\":\"type\"}]}";
 	
+	private JSONStore store;
 	
-	public static void main(String[] args)
-	{
-		JSONStore store=new StubJSONStore("/");
-		boolean success = true;
-		
-		// Test 1: write a json object to a file
-		try
-		{
-			JSONObject jsonObject = new JSONObject(testStr);
-			store.updateJSON("/schema/json1.test", jsonObject);
-			System.out.println("test 1 succeeded.");
+	@Before public void setup() {
+		File tmp=new File(System.getProperty("java.io.tmpdir"));
+		File dir=new File(tmp,"ju-cspace");
+		if(!dir.exists())
+			dir.mkdir();
+		// XXX do it properly when we have delete
+		for(File f : dir.listFiles()) {
+			f.delete();
 		}
-		catch (JSONException je)
-		{
-			System.out.println("test 1 failed.");
-			System.out.println("JSON Exception creating the JSON object from string: " + je);
-			success = false;
-		} catch (ExistException e) {
-			System.out.println("test 1 failed.");
-			System.out.println("ExistException: " + e);
-			success = false;
-		}
-		
-		// test 2: retrieve json object from file
-		try
-		{
-			String result = store.retrieveJson("json1.test");
-			JSONObject resultObj = new JSONObject(result);
-			JSONObject testObj = new JSONObject(testStr);
-			if (resultObj.toString().equals(testObj.toString()))
-			{
-				System.out.println("test 2 succeeded.");
-			}
-			else
-			{
-				System.out.println("test 2 failed.");
-				success = false;
-			}
-		}
-		catch (JSONException je)
-		{
-			System.out.println("JSON exception creating objects for comparison. " + je);
-			System.out.println("test 2 failed.");
-			success = false;
-		}
-		catch (Exception onfe)
-		{
-			System.out.println("Object not found. " + onfe);
-			System.out.println("test 2 failed.");
-			success = false;
-		}
+		store=new StubJSONStore(dir.toString());
+	}
 
-		// test 3: fail to retrieve non-existing json object
+	
+	@Test public void writeJSONToFile() throws JSONException, ExistException {
+		JSONObject jsonObject = new JSONObject(testStr);
+		store.createJSON("/objects/json1.test", jsonObject);
+	}
+	
+	@Test public void readJSONFromFile() throws JSONNotFoundException, JSONException, ExistException {
+		JSONObject jsonObject = new JSONObject(testStr);
+		store.createJSON("/objects/json1.test", jsonObject);
+		String result = store.retrieveJson("/objects/json1.test");
+		JSONObject resultObj = new JSONObject(result);
+		JSONObject testObj = new JSONObject(testStr);
+		assertTrue(resultObj.toString().equals(testObj.toString()));
+	}
+
+	@Test public void testJSONNotExist() throws JSONException {
 		try
 		{
 			String result = store.retrieveJson("nonesuch.json");
 			new JSONObject(result);
-			System.out.println("test 3 failed.");
-			success = false;
+			assertTrue(false);
 		}
-		catch (JSONNotFoundException onfe)
-		{
-			System.out.println("test 3 succeeded.");
-		}
-		catch (Exception e)
-		{
-			System.out.println("Unexpected exception: " + e);
-			System.out.println("test 3 failed.");
-			success = false;
-		}
-
-		System.out.print("Testing completed ");
-		if (success)
-		{
-			System.out.println("successfully");
-		}
-		else
-		{
-			System.out.println("with some failures");
-		}
-		
-	}
-		
+		catch (JSONNotFoundException onfe) {}
+	}		
 }
