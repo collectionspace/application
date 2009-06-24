88703a3b4cd16788eb7b1a5071fdb60d69e71956
Dan Sheppard <dan.sheppard@caret.cam.ac.uk>
2009-06-24 14:16:35 +0000

    NOJIRA sorting out SVN weirdness in my Eclipse which seems thoroughly confused. Hopefully this won't touch trunk.

--- chain/src/main/java/org/collectionspace/chain/controller/ChainServlet.java
+++ chain/src/main/java/org/collectionspace/chain/controller/ChainServlet.java
@@ -3,8 +3,6 @@ package org.collectionspace.chain.controller;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
