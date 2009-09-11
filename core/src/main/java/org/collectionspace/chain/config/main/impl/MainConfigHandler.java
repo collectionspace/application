bdf8f28d0b19ed98284f68be37e7c5379d1ea1ac
Dan Sheppard <dan.sheppard@caret.cam.ac.uk>
2009-09-11 17:42:39 +0000

    NOJIRA core now uses CSP helper where appropriate.

--- core/src/main/java/org/collectionspace/chain/config/main/impl/MainConfigHandler.java
+++ core/src/main/java/org/collectionspace/chain/config/main/impl/MainConfigHandler.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.collectionspace.csp.api.config.EventContext;
 import org.collectionspace.csp.api.config.EventConsumer;
+import org.collectionspace.csp.helper.config.impl.EventContextImpl;
 import org.xml.sax.Attributes;
 import org.xml.sax.helpers.DefaultHandler;
 
