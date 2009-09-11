160ca75eb2dfc8e5da7c70b6ed5e2da6d588b2d0
Dan Sheppard <dan.sheppard@caret.cam.ac.uk>
2009-09-11 20:09:18 +0000

    NOJIRA more kludges, :(.

---
+++ svcapp/csp-api/src/main/java/org/collectionspace/kludge/JSONTestUtil.java
@@ -0,0 +1,42 @@
+package org.collectionspace.kludge;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Iterator;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class JSONTestUtil {
+	// XXX refactor
+	@SuppressWarnings("unchecked")
+	public static void assertJSONEquiv(Object a,Object b) throws JSONException {
+		if(a==null) {
+			assertNull(b);
+			return;
+		}
+		if((a instanceof Number) || (a instanceof Boolean) || (a instanceof String)) {
+			assertEquals(a,b);
+			return;
+		}
+		if(a instanceof JSONArray) {
+			assertTrue(b instanceof JSONArray);
+			assertEquals(((JSONArray) a).length(),((JSONArray)b).length());
+			for(int i=0;i<((JSONArray) a).length();i++)
+				assertJSONEquiv(((JSONArray) a).get(i),((JSONArray) b).get(i));
+			return;
+		}
+		assertTrue(a instanceof JSONObject);
+		assertTrue(b instanceof JSONObject);
+		assertEquals(((JSONObject)a).length(),((JSONObject)b).length());
+		Iterator t=((JSONObject)a).keys();
+		while(t.hasNext()) {
+			String key=(String)t.next();
+			assertTrue(((JSONObject)b).has(key));
+			assertJSONEquiv(((JSONObject)a).get(key),((JSONObject)b).get(key));
+		}
+	}
+}
