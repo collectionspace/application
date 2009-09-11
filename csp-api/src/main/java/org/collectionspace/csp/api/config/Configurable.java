60b3c7a4ee2c112adb365cdca5efddf3b186b316
Dan Sheppard <dan.sheppard@caret.cam.ac.uk>
2009-09-11 13:46:47 +0000

    NOJIRA Separate CSP API jar.

--- csp-api/src/main/java/org/collectionspace/csp/api/config/Configurable.java
+++ csp-api/src/main/java/org/collectionspace/csp/api/config/Configurable.java
@@ -1,10 +1,8 @@
 package org.collectionspace.csp.api.config;
 
-import org.collectionspace.chain.config.bootstrap.BootstrapConfigController;
-import org.collectionspace.chain.config.main.ConfigRoot;
 import org.collectionspace.csp.api.core.CSPDependencyException;
 
 public interface Configurable {
-	public void configure(BootstrapConfigController bootstrap,ConfigRoot config) throws CSPDependencyException;
+	public void configure(Object bootstrap,Object config) throws CSPDependencyException;
 	public String getName();
 }
--- csp-api/src/main/java/org/collectionspace/csp/api/container/CSPManager.java
+++ csp-api/src/main/java/org/collectionspace/csp/api/container/CSPManager.java
@@ -1,7 +1,5 @@
 package org.collectionspace.csp.api.container;
 
-import org.collectionspace.chain.config.api.ConfigLoadFailedException;
-import org.collectionspace.chain.config.bootstrap.BootstrapConfigController;
 import org.collectionspace.csp.api.core.CSP;
 import org.collectionspace.csp.api.core.CSPContext;
 import org.collectionspace.csp.api.core.CSPDependencyException;
@@ -11,6 +9,6 @@ import org.xml.sax.InputSource;
 public interface CSPManager extends CSPContext {
 	public void register(CSP in);
 	public void go() throws CSPDependencyException;
-	public void configure(BootstrapConfigController bootstrap,InputSource in,String url) throws ConfigLoadFailedException, CSPDependencyException;
+	public void configure(Object bootstrap,InputSource in,String url) throws CSPDependencyException;
 	public Storage getStorage(String name);
 }
