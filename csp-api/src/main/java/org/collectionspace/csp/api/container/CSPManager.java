1d956bc6e12645729a3327d4acbac43b3750d95e
Dan Sheppard <dan.sheppard@caret.cam.ac.uk>
2009-09-11 19:09:00 +0000

    NOJIRA csp-api changes to support kludges pending reorg.

--- csp-api/src/main/java/org/collectionspace/csp/api/container/CSPManager.java
+++ csp-api/src/main/java/org/collectionspace/csp/api/container/CSPManager.java
@@ -4,11 +4,12 @@ import org.collectionspace.csp.api.core.CSP;
 import org.collectionspace.csp.api.core.CSPContext;
 import org.collectionspace.csp.api.core.CSPDependencyException;
 import org.collectionspace.csp.api.persistence.Storage;
+import org.collectionspace.kludge.BCCKludge;
 import org.xml.sax.InputSource;
 
 public interface CSPManager extends CSPContext {
 	public void register(CSP in);
 	public void go() throws CSPDependencyException;
-	public void configure(Object bootstrap,InputSource in,String url) throws CSPDependencyException;
+	public void configure(BCCKludge bootstrap,InputSource in,String url) throws CSPDependencyException;
 	public Storage getStorage(String name);
 }
