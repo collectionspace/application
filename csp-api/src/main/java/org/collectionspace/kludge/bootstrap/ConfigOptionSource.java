ee8da69493c4ba137e783cd6a4ddfe72136f8ada
Dan Sheppard <dan.sheppard@caret.cam.ac.uk>
2009-09-11 21:14:30 +0000

    NOJIRA more kludges for refactor.

---
+++ csp-api/src/main/java/org/collectionspace/kludge/bootstrap/ConfigOptionSource.java
@@ -0,0 +1,31 @@
+/* Copyright 2009 University of Cambridge
+ * Licensed under the Educational Community License (ECL), Version 2.0. You may not use this file except in 
+ * compliance with this License.
+ *
+ * You may obtain a copy of the ECL 2.0 License at https://source.collectionspace.org/collection-space/LICENSE.txt
+ */
+package org.collectionspace.kludge.bootstrap;
+
+import org.dom4j.Element;
+
+/** [package scope] internal use, a part of the control file */
+class ConfigOptionSource {
+	private Element element;
+	private ConfigLoadMethod method;
+	private String value;
+	
+	ConfigOptionSource(ConfigLoadMethod method,Element element) {
+		this.method=method;
+		this.element=element;
+	}
+	
+	public String getString() {
+		if(value==null) {
+			synchronized(this) {
+				if(value==null)
+					value=method.getString(element);
+			}
+		}
+		return value;
+	}
+}
---
+++ csp-api/src/main/java/org/collectionspace/kludge/bootstrap/DefaultConfigLoadMethod.java
@@ -0,0 +1,20 @@
+/* Copyright 2009 University of Cambridge
+ * Licensed under the Educational Community License (ECL), Version 2.0. You may not use this file except in 
+ * compliance with this License.
+ *
+ * You may obtain a copy of the ECL 2.0 License at https://source.collectionspace.org/collection-space/LICENSE.txt
+ */
+package org.collectionspace.kludge.bootstrap;
+
+import org.collectionspace.kludge.ConfigLoadFailedException;
+import org.dom4j.Document;
+import org.dom4j.Element;
+
+/** method to just return the string provided (a fallback) */
+public class DefaultConfigLoadMethod implements ConfigLoadMethod {
+
+	public String getString(Element e) { return e.getTextTrim(); }
+
+	public void init(BootstrapConfigController controller, Document root)
+			throws ConfigLoadFailedException {}
+}
