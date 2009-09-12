cf14cd74c890c5f63524f3d84447df44b8b0bf53
Dan Sheppard <dan.sheppard@caret.cam.ac.uk>
2009-09-12 16:02:07 +0000

    NOJIRA removing JSONTestUtil kludge. Commit 5/5.

---
+++ jxutils/src/main/java/org/collectionspace/chain/util/json/JSONUtils.java
@@ -0,0 +1,48 @@
+package org.collectionspace.chain.util.json;
+
+import java.util.Iterator;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class JSONUtils {
+	// XXX refactor
+	@SuppressWarnings("unchecked")
+	public static boolean checkJSONEquiv(Object a,Object b) throws JSONException {
+		if(a==null) {
+			if(b!=null)
+				return false;
+			return true;
+		}
+		if((a instanceof Number) || (a instanceof Boolean) || (a instanceof String)) {
+			if(!a.equals(b))
+				return false;
+			return true;
+		}
+		if(a instanceof JSONArray) {
+			if(!(b instanceof JSONArray))
+				return false;
+			if(((JSONArray)a).length()!=((JSONArray)b).length())
+				return false;
+			for(int i=0;i<((JSONArray) a).length();i++) {
+				if(!checkJSONEquiv(((JSONArray) a).get(i),((JSONArray) b).get(i)))
+					return false;
+			}
+			return true;
+		}
+		if(!(a instanceof JSONObject) || !(b instanceof JSONObject))
+			return false;
+		if(((JSONObject)a).length()!=((JSONObject)b).length())
+			return false;
+		Iterator t=((JSONObject)a).keys();
+		while(t.hasNext()) {
+			String key=(String)t.next();
+			if(!((JSONObject)b).has(key))
+				return false;
+			if(!checkJSONEquiv(((JSONObject)a).get(key),((JSONObject)b).get(key)))
+				return false;
+		}
+		return true;
+	}
+}
