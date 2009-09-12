e2839df6bc94498c18e63413652161d98e37ae64
Dan Sheppard <dan.sheppard@caret.cam.ac.uk>
2009-09-12 16:15:03 +0000

    NOJIRA Removal of ConfigRoot kludge commit 3/4.

--- cspi-file/src/main/java/org/collectionspace/chain/csp/persistence/file/FileStorage.java
+++ cspi-file/src/main/java/org/collectionspace/chain/csp/persistence/file/FileStorage.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import org.collectionspace.csp.api.config.BarbWirer;
 import org.collectionspace.csp.api.config.ConfigConsumer;
 import org.collectionspace.csp.api.config.ConfigContext;
+import org.collectionspace.csp.api.config.ConfigRoot;
 import org.collectionspace.csp.api.config.Configurable;
 import org.collectionspace.csp.api.core.CSP;
 import org.collectionspace.csp.api.core.CSPContext;
@@ -62,7 +63,7 @@ public class FileStorage extends ProxyStorage implements Storage, CSP, ConfigCon
 		}
 	}
 
-	public void configure(CRKludge config) throws CSPDependencyException {
+	public void configure(ConfigRoot config) throws CSPDependencyException {
 		Object store=config.getValue(new Object[]{"bootstrap","store"});
 		if(store!=null && (store instanceof String)) {
 			real_init((String)store);
