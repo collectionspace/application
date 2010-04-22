3143b67e79813b92d4ac5e6efc1a624a2a5e01a0
Chris Martin <csm22@cam.ac.uk>
2010-04-22 16:52:46 +0000

    NOJIRA logstatus call

---
+++ cspi-webui/src/main/java/org/collectionspace/chain/csp/webui/misc/WebLoginStatus.java
@@ -0,0 +1,38 @@
+package org.collectionspace.chain.csp.webui.misc;
+
+import org.collectionspace.chain.csp.schema.Spec;
+import org.collectionspace.chain.csp.webui.main.Request;
+import org.collectionspace.chain.csp.webui.main.WebMethod;
+import org.collectionspace.chain.csp.webui.main.WebUI;
+import org.collectionspace.csp.api.ui.UIException;
+import org.collectionspace.csp.api.ui.UIRequest;
+import org.collectionspace.csp.api.ui.UISession;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class WebLoginStatus  implements WebMethod {
+	
+	public void testlogin(Request in) throws UIException {
+		JSONObject output= new JSONObject();
+		UIRequest request=in.getUIRequest();
+		try {
+			if(request.getSession().getValue(UISession.USERID).equals("")){
+				output.put("login", false);
+			}
+			else{
+				output.put("login", true);
+			}
+			request.sendJSONResponse(output);
+		} catch (JSONException x) {
+			throw new UIException("Failed to parse json: "+x,x);
+		}
+	}
+	
+	public void run(Object in,String[] tail) throws UIException {
+		testlogin((Request)in);
+	}
+
+	public void configure(WebUI ui, Spec spec) {
+		
+	}
+}
