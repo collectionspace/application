bb78b693582bb5554a0d158ed9d82c4251da42f0
Chris Martin <csm22@cam.ac.uk>
2010-06-02 11:39:07 +0000

    CSPACE-1491

--- cspi-services/src/main/java/org/collectionspace/chain/csp/persistence/services/vocab/ConfiguredVocabStorage.java
+++ cspi-services/src/main/java/org/collectionspace/chain/csp/persistence/services/vocab/ConfiguredVocabStorage.java
@@ -125,7 +125,7 @@ public class ConfiguredVocabStorage implements ContextualisedStorage {
 	throws ExistException, UnimplementedException, UnderlyingStorageException {
 		try {			
 			String vocab=vocab_cache.getVocabularyId(creds,cache,filePath.split("/")[0]);
-			int status=conn.getNone(RequestMethod.DELETE,generateURL(vocab,filePath.split("/")[1],false),null,creds,cache);
+			int status=conn.getNone(RequestMethod.DELETE,generateURL(vocab,filePath.split("/")[1],""),null,creds,cache);
 			if(status>299)
 				throw new UnderlyingStorageException("Could not retrieve vocabulary status="+status);
 			cache.removeCached(getClass(),new String[]{"namefor",vocab,filePath.split("/")[1]});
@@ -257,11 +257,10 @@ public class ConfiguredVocabStorage implements ContextualisedStorage {
 		}
 	}
 
-	private String generateURL(String vocab,String path,Boolean refObjs) throws ExistException, ConnectionException, UnderlyingStorageException {
-		if(refObjs)
-			return r.getServicesURL()+"/"+vocab+"/items/"+path+"/refObjs";
-		else
-			return r.getServicesURL()+"/"+vocab+"/items/"+path;
+	private String generateURL(String vocab,String path,String extrapath) throws ExistException, ConnectionException, UnderlyingStorageException {
+		String url = r.getServicesURL()+"/"+vocab+"/items/"+path+extrapath;
+		return url;
+		
 	}
 
 	private JSONObject urnGet(String vocab,String entry,String refname) throws JSONException, ExistException, UnderlyingStorageException {
@@ -295,9 +294,11 @@ public class ConfiguredVocabStorage implements ContextualisedStorage {
 				*/
 				
 				if(path[path.length-1].equals("refObjs")){
-					out=get(creds,cache,vocab,csid,true);
+					out=refObjViewRetrieveJSON(root, creds, cache, vocab, csid);
+				}else if(path[path.length-1].equals("authorityrefs")){
+					out=refViewRetrieveJSON(root, creds, cache, vocab, csid);
 				}else{
-					out=get(creds,cache,vocab,csid,false);
+					out=get(creds,cache,vocab,csid);
 					// XXX actually use cache			
 					cache.setCached(getClass(),new String[]{"namefor",vocab,csid},out.get(getDisplayNameKey()));
 					cache.setCached(getClass(),new String[]{"reffor",vocab,csid},out.get("refid"));
@@ -310,12 +311,11 @@ public class ConfiguredVocabStorage implements ContextualisedStorage {
 		}
 	}
 	
-	private JSONObject get(CSPRequestCredentials creds,CSPRequestCache cache,String vocab,String csid, Boolean refObjs) throws ConnectionException, ExistException, UnderlyingStorageException, JSONException {
+	private JSONObject get(CSPRequestCredentials creds,CSPRequestCache cache,String vocab,String csid) throws ConnectionException, ExistException, UnderlyingStorageException, JSONException {
 		int status=0;
 		JSONObject out = new JSONObject();
-		if(!refObjs){
 			// XXX pagination support
-			ReturnedMultipartDocument doc=conn.getMultipartXMLDocument(RequestMethod.GET,generateURL(vocab,csid,refObjs),null,creds,cache);
+			ReturnedMultipartDocument doc=conn.getMultipartXMLDocument(RequestMethod.GET,generateURL(vocab,csid,""),null,creds,cache);
 			if(doc.getStatus()==404)
 				throw new ExistException("Does not exist");
 			if(doc.getStatus()>299)
@@ -339,12 +339,53 @@ public class ConfiguredVocabStorage implements ContextualisedStorage {
 			out.put("authorityid", vocab);
 			out.put("recordtype",r.getWebURL());
 			return out;
-		}else{
-			ReturnedDocument doc=conn.getXMLDocument(RequestMethod.GET,generateURL(vocab,csid,refObjs),null,creds,cache);
-			convertToJson(out, doc.getDocument());
-			return out;
-		}
+	}
+	
+	public JSONObject refObjViewRetrieveJSON(ContextualisedStorage storage,CSPRequestCredentials creds,CSPRequestCache cache,String vocab,String csid) throws ConnectionException, ExistException, UnderlyingStorageException, JSONException {
 		
+		JSONObject out=new JSONObject();
+		ReturnedDocument all = conn.getXMLDocument(RequestMethod.GET,generateURL(vocab,csid,"/refObjs"),null,creds,cache);
+		convertToJson(out, all.getDocument());
+		return out;
+	}
+
+	// XXX support URNs for reference
+	private JSONObject miniForURI(ContextualisedStorage storage,CSPRequestCredentials creds,CSPRequestCache cache,String refname,String uri) throws ExistException, UnimplementedException, UnderlyingStorageException, JSONException {
+		return storage.retrieveJSON(storage,creds,cache,"direct/urn/"+uri+"/"+refname);
+	}
+	
+	public JSONObject refViewRetrieveJSON(ContextualisedStorage storage,CSPRequestCredentials creds,CSPRequestCache cache,String vocab,String csid) throws ExistException,UnimplementedException, UnderlyingStorageException, JSONException {
+		try {
+			JSONObject out=new JSONObject();
+			//not all the records need a reference, look in default.xml for which that don't
+			if(r.hasTermsUsed()){
+				String path = generateURL(vocab,csid,"/authorityrefs");
+				ReturnedDocument all = conn.getXMLDocument(RequestMethod.GET,path,null,creds,cache);
+				if(all.getStatus()!=200)
+					throw new ConnectionException("Bad request during identifier cache map update: status not 200");
+				Document list=all.getDocument();
+				for(Object node : list.selectNodes("authority-ref-list/authority-ref-item")) {
+					if(!(node instanceof Element))
+						continue;
+					String key=((Element)node).selectSingleNode("sourceField").getText();
+					String uri=((Element)node).selectSingleNode("uri").getText();
+					String refname=((Element)node).selectSingleNode("refName").getText();
+					String fieldName = key.split(":")[1];
+					Field fieldinstance = (Field)r.getRepeatField(fieldName);
+					
+					if(uri!=null && uri.startsWith("/"))
+						uri=uri.substring(1);
+					JSONObject data=miniForURI(storage,creds,cache,refname,uri);
+					data.put("sourceFieldselector", fieldinstance.getSelector());
+					data.put("sourceFieldName", fieldName);
+					data.put("sourceFieldType", r.getID());
+					out.put(key,data);
+				}
+			}
+			return out;
+		} catch (ConnectionException e) {
+			throw new UnderlyingStorageException("Connection problem",e);
+		}
 	}
 	
 	private void convertToJson(JSONObject out,Document in) throws JSONException {
@@ -364,7 +405,7 @@ public class ConfiguredVocabStorage implements ContextualisedStorage {
 				String[] tag_path=record_path[1].split(",",2);
 				body.put(record_path[0],createEntry(section,tag_path[0],tag_path[1],jsonObject,vocab,refname));
 			}
-			ReturnedMultipartDocument out=conn.getMultipartXMLDocument(RequestMethod.PUT,generateURL(vocab,filePath.split("/")[1],false),body,creds,cache);
+			ReturnedMultipartDocument out=conn.getMultipartXMLDocument(RequestMethod.PUT,generateURL(vocab,filePath.split("/")[1],""),body,creds,cache);
 			if(out.getStatus()>299)
 				throw new UnderlyingStorageException("Could not create vocabulary status="+out.getStatus());
 			cache.setCached(getClass(),new String[]{"namefor",vocab,filePath.split("/")[1]},name);
