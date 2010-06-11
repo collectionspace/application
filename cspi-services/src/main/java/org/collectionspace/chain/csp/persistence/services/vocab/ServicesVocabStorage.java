0bdc136dab3fa3473463264e214ac81ce4943601
Chris Martin <csm22@cam.ac.uk>
2010-06-11 09:32:13 +0000

    CSPACE-1967 - allow for initialization of vocab lists

--- cspi-services/src/main/java/org/collectionspace/chain/csp/persistence/services/vocab/ServicesVocabStorage.java
+++ cspi-services/src/main/java/org/collectionspace/chain/csp/persistence/services/vocab/ServicesVocabStorage.java
@@ -15,8 +15,17 @@ public class ServicesVocabStorage extends GenericVocabStorage {
 	static {
 		vocabs.put("xxx","Temporary Vocab whilst none exist");
 	}
-	
+
 	public ServicesVocabStorage(ServicesConnection conn) throws InvalidXTmplException, DocumentException {
+		super(conn,"urn:cspace:org.collectionspace.demo:vocabulary({vocab}):item({entry})'{display}'",
+			  Pattern.compile("(.*?)/urn:cspace:org.collectionspace.demo:vocabulary\\((.*?)\\):item\\((.*?)\\)'(.*?)'"),
+			  vocabs,"http://collectionspace.org/services/vocabulary","vocabularies","vocabularies_common","vocabularyitems_common",
+			  "vocabularies-common-list/vocabulary-list-item","vocabularyitems-common-list/vocabularyitem_list_item",
+			  "vocabularyitems_common/displayName","vocabularies_common","inVocabulary");
+	}
+	
+	//allow for initialization of vocab from ServiceStorageGenerator
+	public ServicesVocabStorage(ServicesConnection conn, Map<String,String> vocabs) throws InvalidXTmplException, DocumentException {
 		super(conn,"urn:cspace:org.collectionspace.demo:vocabulary({vocab}):item({entry})'{display}'",
 			  Pattern.compile("(.*?)/urn:cspace:org.collectionspace.demo:vocabulary\\((.*?)\\):item\\((.*?)\\)'(.*?)'"),
 			  vocabs,"http://collectionspace.org/services/vocabulary","vocabularies","vocabularies_common","vocabularyitems_common",
