53def102f8c7d1f09bdde6879d1c2e9ae982ecfc
Chris Martin <csm22@cam.ac.uk>
2010-08-25 08:44:00 +0000

    CSPACE-1817

--- cspi-webui/src/main/java/org/collectionspace/chain/csp/webui/misc/VocabRedirector.java
+++ cspi-webui/src/main/java/org/collectionspace/chain/csp/webui/misc/VocabRedirector.java
@@ -1,6 +1,7 @@
 package org.collectionspace.chain.csp.webui.misc;
 
 import org.collectionspace.chain.csp.schema.Field;
+import org.collectionspace.chain.csp.schema.Instance;
 import org.collectionspace.chain.csp.schema.Record;
 import org.collectionspace.chain.csp.schema.Spec;
 import org.collectionspace.chain.csp.webui.main.Request;
@@ -10,6 +11,7 @@ import org.collectionspace.csp.api.core.CSPRequestCache;
 import org.collectionspace.csp.api.persistence.Storage;
 import org.collectionspace.csp.api.ui.UIException;
 import org.collectionspace.csp.api.ui.UIRequest;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
@@ -23,16 +25,32 @@ public class VocabRedirector implements WebMethod {
 	
 	public void configure(WebUI ui, Spec spec) {}
 
+	//old style
 	private String pathFor(String in) {
 		Field fd = (Field) r.getRepeatField(in);
 		String weburl = fd.getAutocompleteInstance().getWebURL();
+		
 		return "/vocabularies/"+weburl; 
 	}
 	
+	private JSONArray pathForAll(String fieldname) throws JSONException{
+		JSONArray out = new JSONArray();
+
+		Field fd = (Field) r.getRepeatField(fieldname);
+		for(Instance autoc : fd.getAllAutocompleteInstances()){
+			JSONObject instance = new JSONObject();
+			instance.put("url","/vocabularies/"+autoc.getWebURL());
+			instance.put("type",autoc.getID());
+			instance.put("fullName",autoc.getTitle());
+			out.put(instance);
+		}
+		
+		return out;
+	}
 	private void redirect(CSPRequestCache cache,Storage storage,UIRequest request,String[] tail) throws UIException {
 		try {
-			JSONObject out=new JSONObject();
-			out.put("url",pathFor(tail[0]));
+			JSONArray out = new JSONArray();
+			out = pathForAll(tail[0]);
 			request.sendJSONResponse(out);
 		} catch (JSONException e) {
 			throw new UIException("JSON building failed",e);
