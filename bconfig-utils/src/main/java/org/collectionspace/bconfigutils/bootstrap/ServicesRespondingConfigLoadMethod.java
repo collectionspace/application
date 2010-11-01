936bc0dcfb1a3d8db9050a4f802ebb88a571026b
Dan Sheppard <dan.sheppard@caret.cam.ac.uk>
2010-11-01 14:48:00 +0000

    NOJIRA Update host detection to remove scary but actually unproblematic "host not found" message from console.

--- bconfig-utils/src/main/java/org/collectionspace/bconfigutils/bootstrap/ServicesRespondingConfigLoadMethod.java
+++ bconfig-utils/src/main/java/org/collectionspace/bconfigutils/bootstrap/ServicesRespondingConfigLoadMethod.java
@@ -6,16 +6,42 @@
  */
 package org.collectionspace.bconfigutils.bootstrap;
 
+import java.net.MalformedURLException;
+import java.net.Socket;
+import java.net.URL;
+
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.dom4j.Document;
 import org.dom4j.Element;
+import org.mortbay.log.Log;
 
 /** Expects a URL, but will only return it if the service layer is working at this URL */
-// XXX CSPACE-302
-public class ServicesRespondingConfigLoadMethod implements ConfigLoadMethod {
+// XXX CSPACE-88
+public class ServicesRespondingConfigLoadMethod implements ConfigLoadMethod {	
+	private boolean ping_socket(String host,int port) {
+		try {
+			Socket s=new Socket(host,port);
+			s.close();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	
 	private boolean pings(String url) {
+		/* To avoid irritating messages from high-level protocols, we try a socket connect first.
+		 */
+		URL urld;
+		try {
+			urld = new URL(url);
+		} catch (MalformedURLException e) {
+			Log.warn("Bad URL "+e);
+			return false;
+		}
+		if(!ping_socket(urld.getHost(),urld.getPort()))
+			return false;
 		HttpClient client=new HttpClient();
 		HttpMethod method=new GetMethod(url);
 		try {
