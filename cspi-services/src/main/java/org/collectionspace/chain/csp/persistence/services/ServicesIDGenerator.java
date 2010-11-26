cc2b8be0d1b14ecfafd6206fa3fa99b9f6ca1077
Chris Martin <csm22@cam.ac.uk>
2010-11-26 09:25:23 +0000

    CSPACE-3177

--- cspi-services/src/main/java/org/collectionspace/chain/csp/persistence/services/ServicesIDGenerator.java
+++ cspi-services/src/main/java/org/collectionspace/chain/csp/persistence/services/ServicesIDGenerator.java
@@ -6,10 +6,8 @@
  */
 package org.collectionspace.chain.csp.persistence.services;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 
 import org.collectionspace.chain.csp.persistence.services.connection.ConnectionException;
 import org.collectionspace.chain.csp.persistence.services.connection.RequestMethod;
@@ -20,7 +18,6 @@ import org.collectionspace.chain.csp.schema.Spec;
 import org.collectionspace.csp.api.core.CSPRequestCache;
 import org.collectionspace.csp.api.core.CSPRequestCredentials;
 import org.collectionspace.csp.api.persistence.ExistException;
-import org.collectionspace.csp.api.persistence.Storage;
 import org.collectionspace.csp.api.persistence.UnderlyingStorageException;
 import org.collectionspace.csp.api.persistence.UnimplementedException;
 import org.collectionspace.csp.helper.persistence.ContextualisedStorage;
@@ -28,37 +25,12 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class ServicesIDGenerator implements ContextualisedStorage {
-	private Random rnd=new Random();
 	private ServicesConnection conn;
-	private Spec spec;
 	
 	private static final Map<String,String> generators=new HashMap<String,String>();
 
-	/*these keys are the primary keys in the id_generators table in the service layer.
-	while using these keys we get more information about the type of the object (like the prefix for the generated id)
-	static {
-		generators.put("accession-activity","1a67470b-19b1-4ae3-88d4-2a0aa936270e");
-		generators.put("objects","9dd92952-c384-44dc-a736-95e435c1759c"); // XXX temporary hack for venus
-		generators.put("accession","9dd92952-c384-44dc-a736-95e435c1759c");
-		generators.put("archive","70586d30-9dca-4a07-a3a2-1976fe898028");
-		generators.put("archives","70586d30-9dca-4a07-a3a2-1976fe898028");// XXX temporary hack for venus
-		generators.put("evaluation","d2d80822-25c7-4c7c-a105-fc40cdb0c50f");
-		generators.put("evaluations","d2d80822-25c7-4c7c-a105-fc40cdb0c50f");// XXX temporary hack for venus
-		generators.put("intake","8088cfa5-c743-4824-bb4d-fb11b12847f7");
-		generators.put("intake-object","a91db555-5c53-4996-9918-6712351397a0");
-		generators.put("library","80fedaf6-1647-4f30-9f53-a75a3cac2ffd");
-		generators.put("loanin","ed87e7c6-0678-4f42-9d33-f671835586ef");
-		generators.put("loans-in","ed87e7c6-0678-4f42-9d33-f671835586ef");
-		generators.put("loanout", "4b984865-f93d-4481-b874-3dba863ec589");
-		generators.put("loans-out", "4b984865-f93d-4481-b874-3dba863ec589");
-		generators.put("study","0518132e-dd8c-4773-8fa9-07c9af4444ee");
-		generators.put("uuid","1fa40353-05b8-4ae6-82a6-44a18b4f3c12");
-		generators.put("movement", "49ca9d8d-7136-47ff-a70e-4a47b9038b70");
-	}
-*/
 	public ServicesIDGenerator(ServicesConnection conn, Spec spec) { 
 		this.conn=conn;
-		this.spec= spec;
 		Record idgenerator = spec.getRecord("id");
 		for(FieldSet fs: idgenerator.getAllFields("")){
 			String name = fs.getID();
