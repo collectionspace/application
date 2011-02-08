9e64eb68b5f2bdcfff6a2efb54053a853f3d74ca
Chris Martin <csm22@cam.ac.uk>
2011-02-08 11:16:22 +0000

    CSPACE-3410

--- cspi-webui/src/main/java/org/collectionspace/chain/csp/webui/external/UIMapping.java
+++ cspi-webui/src/main/java/org/collectionspace/chain/csp/webui/external/UIMapping.java
@@ -39,6 +39,35 @@ public class UIMapping {
 		uimetaConfigs.put(metaconfig.getId(), metaconfig);
 	}
 
+	public boolean hasMetaConfig(){
+		if(uimetaConfigs.isEmpty()){
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean hasMetaConfig(String name){
+		if(uimetaConfigs.containsKey(name)){
+			return true;
+		}
+		return false;
+	}
+
+	public UIMeta getMetaConfig(String name){
+		if( uimetaConfigs.containsKey(name)){
+			return uimetaConfigs.get(name);
+		}
+		return null;
+	}
+	
+	public String[] getAllMetaConfigs(){
+
+		if (uimetaConfigs.isEmpty()) {
+			return new String[0];
+		}
+		return uimetaConfigs.keySet().toArray(new String[0]);
+	}
+	
 	public Boolean hasFile() {
 		if (file.equals("")) {
 			return false;
