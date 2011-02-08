496139fe88bd6bb264448d132d614a46f5a66333
Chris Martin <csm22@cam.ac.uk>
2011-02-08 11:17:33 +0000

    CSPACE-3410

--- tomcat-main/src/main/java/org/collectionspace/chain/controller/ChainUIServlet.java
+++ tomcat-main/src/main/java/org/collectionspace/chain/controller/ChainUIServlet.java
@@ -1,15 +1,38 @@
 package org.collectionspace.chain.controller;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.List;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Node;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.io.input.TeeInputStream;
 import org.collectionspace.chain.csp.config.ConfigRoot;
+import org.collectionspace.chain.csp.persistence.services.connection.ConnectionException;
+import org.collectionspace.chain.csp.persistence.services.connection.ConnectionUtils;
 import org.collectionspace.chain.csp.schema.Record;
 import org.collectionspace.chain.csp.schema.Spec;
 import org.collectionspace.chain.csp.webui.external.UIMapping;
@@ -17,6 +40,8 @@ import org.collectionspace.chain.csp.webui.main.WebUI;
 import org.collectionspace.csp.api.ui.UI;
 
 public class ChainUIServlet extends ChainServlet {
+	private static final Logger log = LoggerFactory
+			.getLogger(ChainUIServlet.class);
 
 	/**
 	 * 
@@ -43,30 +68,46 @@ public class ChainUIServlet extends ChainServlet {
 			Spec spec = (Spec) root.getRoot(Spec.SPEC_ROOT);
 
 			if (pathinfo.equals("/html") || pathinfo.equals("/html/")) {
-				servlet_response.sendRedirect("/collectionspace/ui/html/index.html");
-			}
-			
-			String path = pathinfo;
-			for (UIMapping map : allmappings) {
-				if (map.hasUrl()) {
-					// does pathinfo match url
-					if (map.getUrl().equals(pathinfo)) {
-						path = map.getFile();
-					}
-				} else if (map.hasType()) {
-					for (Record r : spec.getAllRecords()) {
-						if (r.isType(map.getType())) {
-							if (pathinfo.equals("/html/" + r.getUIURL())) {
-								path = map.getFile();
-							}
-						}
-					}
-				}
+				servlet_response
+						.sendRedirect("/collectionspace/ui/html/index.html");
 			}
 
 			ServletContext sc = null;
-
 			sc = getServletContext().getContext("/cspace-ui");
+
+			Boolean doMetaConfig = false;
+			String path = pathinfo;
+			UIMapping validmap = doMapping(pathinfo, allmappings, spec);
+			if (null != validmap) {
+				path = validmap.getFile();
+				if (validmap.hasMetaConfig() && doMetaConfig) {
+					InputStream is = sc.getResourceAsStream(path);
+					
+					SAXReader reader = new SAXReader();
+					reader.setEntityResolver(new NullResolver());
+					Document xmlfile = null;
+
+					ByteArrayOutputStream dump = new ByteArrayOutputStream();
+					xmlfile = reader.read(new TeeInputStream(is, dump));
+					log.info(dump.toString("UTF-8"));
+
+					for (String metafield : validmap.getAllMetaConfigs()) {
+						String xPath = "//html/head/" + metafield;
+						List<Node> nodes = xmlfile.selectNodes(xPath);
+
+						if (!nodes.isEmpty()) {
+							String fieldval = validmap.getMetaConfig(metafield)
+									.getValue();
+							nodes.get(0).setText(fieldval);
+						}
+					}
+					InputStream source = documentToStream(xmlfile);
+					serveContent(servlet_response, source);
+
+					is.close();
+					return;
+				}
+			}
 
 			if (serverFixedExternalContent(servlet_request, servlet_response,
 					sc, path)) {
@@ -76,6 +117,69 @@ public class ChainUIServlet extends ChainServlet {
 		} catch (BadRequestException x) {
 			servlet_response.sendError(HttpServletResponse.SC_BAD_REQUEST,
 					getStackTrace(x));
+		} catch (ConnectionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (DocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+	}
+	/* borrowed from ContectionUtils and tweaked e.g. set newline = true*/
+	static InputStream serializetoXML(Document doc) throws IOException {
+		return new ByteArrayInputStream(serializeToBytes(doc));
+	}
+
+	static byte[] serializeToBytes(Document doc) throws IOException {
+		ByteArrayOutputStream out=new ByteArrayOutputStream();
+		OutputFormat outformat = OutputFormat.createPrettyPrint();
+		outformat.setExpandEmptyElements(true);
+		outformat.setNewlines(true);
+		outformat.setIndent(false);
+		XMLWriter writer = new XMLWriter(out, outformat);
+		writer.write(doc);
+		writer.flush();
+		out.close();
+		return out.toByteArray();
+	}
+	
+	public static InputStream documentToStream(Document in) throws ConnectionException {
+		if(in!=null) {
+			try {
+				return serializetoXML(in);
+			} catch (IOException e) {
+				throw new ConnectionException("Could not connect "+e.getLocalizedMessage(),e);
+			}
+		}
+		return null;
+	}
+
+	private UIMapping doMapping(String pathinfo, UIMapping[] allmappings,
+			Spec spec) {
+		for (UIMapping map : allmappings) {
+
+			if (map.hasUrl()) {
+				if (map.getUrl().equals(pathinfo)) {
+					return map;
+				}
+			} else if (map.hasType()) {
+				for (Record r : spec.getAllRecords()) {
+					if (r.isType(map.getType())) {
+						if (pathinfo.equals("/html/" + r.getUIURL())) {
+							return map;
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+}
+
+class NullResolver implements EntityResolver {
+	public InputSource resolveEntity(String publicId, String systemId)
+			throws SAXException, IOException {
+		return new InputSource(new StringReader(""));
 	}
 }
