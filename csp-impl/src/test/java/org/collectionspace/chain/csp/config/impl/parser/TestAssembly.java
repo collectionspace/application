ef6b49adb33009c872869e091182e850953d6d9c
Dan Sheppard <dan.sheppard@caret.cam.ac.uk>
2011-02-09 12:14:06 +0000

    NOJIRA fix broken nightly (jUnit version issue with tests with inner classes).

--- csp-impl/src/test/java/org/collectionspace/chain/csp/config/impl/parser/TestAssembly.java
+++ csp-impl/src/test/java/org/collectionspace/chain/csp/config/impl/parser/TestAssembly.java
@@ -11,6 +11,8 @@ import javax.xml.transform.stream.StreamResult;
 
 
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,18 +20,11 @@ import org.xml.sax.EntityResolver;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
+@RunWith(Enclosed.class)
 public class TestAssembly {
 	private static final Logger log=LoggerFactory.getLogger(TestAssembly.class);
 
 	private static final String assembly1="<?xmlversion=\"1.0\"encoding=\"UTF-8\"?><collection-space><core></core><tag2><tag5><tag6>tag6</tag6></tag5><tag7>tag7</tag7><tag16/><tag18/></tag2></collection-space>";
-	
-	private static final class Resolver implements EntityResolver {
-		@Override public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
-			String path=getClass().getPackage().getName().replaceAll("\\.","/")+"/"+systemId;
-			InputStream in=Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
-			return new InputSource(in);
-		}			
-	}
 
 	
 	//@Test 
