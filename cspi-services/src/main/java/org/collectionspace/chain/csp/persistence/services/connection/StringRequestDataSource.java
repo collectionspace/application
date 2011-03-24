6dd8cabd3969daad9e2fa0415aef219b9fbab225
Chris Martin <csm22@cam.ac.uk>
2011-03-24 16:54:46 +0000

    CSPACE-3741

--- cspi-services/src/main/java/org/collectionspace/chain/csp/persistence/services/connection/StringRequestDataSource.java
+++ cspi-services/src/main/java/org/collectionspace/chain/csp/persistence/services/connection/StringRequestDataSource.java
@@ -14,41 +14,49 @@ import javax.mail.internet.MimeMultipart;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class StringRequestDataSource  implements RequestDataSource {
+public class StringRequestDataSource implements RequestDataSource {
 	private static final Logger log = LoggerFactory
-	.getLogger(StringRequestDataSource.class);
+			.getLogger(StringRequestDataSource.class);
 	private InputStream data;
-	private String contenttype;
 	private String ctype;
 
-	public StringRequestDataSource(byte[] body,String type, String uploadName) throws ConnectionException {
+	public StringRequestDataSource(byte[] body, String type, String uploadName)
+			throws ConnectionException {
 		try {
-			
-			if(body!=null) {
-				MimeMultipart body_mime=new MimeMultipart();
-				InternetHeaders headers=new InternetHeaders();
-				headers.addHeader("Content-Type","image/jpeg");
-				headers.addHeader("Content-Disposition"," form-data; name=\"file\"; filename=\""+uploadName+"\"");
-				BodyPart part=new MimeBodyPart(headers,body);
+
+			if (body != null) {
+				MimeMultipart body_mime = new MimeMultipart();
+				InternetHeaders headers = new InternetHeaders();
+				headers.addHeader("Content-Type", "image/jpeg");
+				headers.addHeader("Content-Disposition",
+						" form-data; name=\"file\"; filename=\"" + uploadName
+								+ "\"");
+				BodyPart part = new MimeBodyPart(headers, body);
 				body_mime.addBodyPart(part);
-				ByteArrayOutputStream indata=new ByteArrayOutputStream();
+				ByteArrayOutputStream indata = new ByteArrayOutputStream();
 				body_mime.writeTo(indata);
 				log.debug("MultiPart request ");
-				data=new ByteArrayInputStream(indata.toByteArray());
-				ctype=body_mime.getContentType();
+				data = new ByteArrayInputStream(indata.toByteArray());
+				ctype = body_mime.getContentType();
 			}
 		} catch (MessagingException e) {
-			throw new ConnectionException("Could not connect"+e.getLocalizedMessage(),e);
+			throw new ConnectionException("Could not connect"
+					+ e.getLocalizedMessage(), e);
 		} catch (IOException e) {
-			throw new ConnectionException("Could not connect"+e.getLocalizedMessage(),e);
-		}	
+			throw new ConnectionException("Could not connect"
+					+ e.getLocalizedMessage(), e);
+		}
 	}
-	
-	public String getMIMEType() { 
-		//multipart/form-data
-		//multipart/mixed
+
+	public String getMIMEType() {
+		// multipart/form-data
+		// multipart/mixed
 		String test = ctype;
 		test = test.replace("multipart/mixed", "multipart/form-data");
-		return test; }
-	public InputStream getStream() { return data; }
+		return test;
+	}
+
+	public InputStream getStream() {
+		return data;
+	}
 }
