74365a442a2f0f09d10fab499f7fe3b79551cd92
Chris Martin <csm22@cam.ac.uk>
2011-06-13 13:45:17 +0000

    nojira - test tenant theory

---
+++ tomcat-main/src/main/java/org/collectionspace/chain/controller/TenantServlet.java
@@ -0,0 +1,250 @@
+package org.collectionspace.chain.controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.collectionspace.chain.csp.config.ConfigRoot;
+import org.collectionspace.chain.csp.inner.CoreConfig;
+import org.collectionspace.chain.csp.persistence.file.FileStorage;
+import org.collectionspace.chain.csp.persistence.services.ServicesStorageGenerator;
+import org.collectionspace.chain.csp.schema.Spec;
+import org.collectionspace.chain.csp.webui.main.WebUI;
+import org.collectionspace.csp.api.core.CSPDependencyException;
+import org.collectionspace.csp.api.ui.UI;
+import org.collectionspace.csp.api.ui.UIException;
+import org.collectionspace.csp.api.ui.UIUmbrella;
+import org.collectionspace.csp.container.impl.CSPManagerImpl;
+import org.collectionspace.csp.helper.core.ConfigFinder;
+import org.dom4j.DocumentException;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+public class TenantServlet extends HttpServlet {
+	private static final Logger log=LoggerFactory.getLogger(TenantServlet.class);
+	private static final long serialVersionUID = -4343156244448081917L;
+	protected Map<String, CSPManagerImpl> tenantCSPM = new HashMap<String, CSPManagerImpl>();
+	protected Map<String, Boolean> tenantInit = new HashMap<String, Boolean>();
+	private Map<String, UIUmbrella> tenantUmbrella = new HashMap<String, UIUmbrella>();
+
+	protected String locked_down=null;
+	
+	/* Not in the constructor because errors during construction of servlets tend to get lost in a mess of startup.
+	 * Better present it on first request.
+	 */
+
+	protected void register_csps(String tenantId) throws IOException, DocumentException {
+		tenantCSPM.get(tenantId).register(new CoreConfig());
+		tenantCSPM.get(tenantId).register(new FileStorage());
+		tenantCSPM.get(tenantId).register(new ServicesStorageGenerator());
+		tenantCSPM.get(tenantId).register(new WebUI());
+		tenantCSPM.get(tenantId).register(new Spec());
+	}
+	
+	protected void load_config(ServletContext ctx, String tenantId) throws CSPDependencyException {
+		try {
+			ConfigFinder cfg=new ConfigFinder(ctx);
+			InputSource cfg_stream = cfg.resolveEntity("-//CSPACE//ROOT","cspace-config-"+tenantId+".xml");
+			if(cfg_stream==null) {
+				locked_down="Cannot find cspace config xml file";
+			} else {
+				tenantCSPM.get(tenantId).configure(cfg_stream,cfg);
+			}
+		} catch (UnsupportedEncodingException e) {
+			throw new CSPDependencyException("Config has bad character encoding",e);
+		} catch (IOException e) {
+			throw new CSPDependencyException("Cannot load config",e);			
+		} catch (SAXException e) {
+			throw new CSPDependencyException("Cannot parse config file",e);			
+		}
+	}
+	
+	protected synchronized void setup(String tenantId) throws BadRequestException {
+		if(tenantInit.get(tenantId))
+			return;
+		try {
+			// Register csps
+			register_csps(tenantId);
+			tenantCSPM.get(tenantId).go(); // Start up CSPs
+			load_config(getServletContext(),tenantId);
+		} catch (IOException e) {
+			throw new BadRequestException("Cannot load config"+e,e);
+		} catch (DocumentException e) {
+			throw new BadRequestException("Cannot load backend"+e,e);
+		} catch (CSPDependencyException e) {
+			throw new BadRequestException("Cannot initialise CSPs"+e,e);
+		}
+		tenantInit.put(tenantId,true);
+	}
+	
+	public void service(HttpServletRequest servlet_request, HttpServletResponse servlet_response) throws ServletException, IOException {
+		try {
+
+			String pathinfo = servlet_request.getPathInfo();
+			String[] pathbits = pathinfo.substring(1).split("/");
+			if (pathbits[0].equals("tenant")) {
+				servlet_response.sendRedirect(pathinfo);
+				return;
+			}
+			
+			String tenantid = pathbits[0];
+			
+			if(locked_down!=null) {
+				//this ended up with a status 200 hmmmm not great so changed it to return a 400... hopefully that wont break anythign else
+
+				servlet_response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Servlet is locked down in a hard fail because of fatal error: "+locked_down);
+				//servlet_response.getWriter().append("Servlet is locked down in a hard fail because of fatal error: "+locked_down);
+				return;
+			}
+			
+			//reinit if url = /chain/init
+			if(pathbits[1].equals("init")){
+				tenantCSPM.put(tenantid, new CSPManagerImpl());
+				tenantInit.put(tenantid, false);
+				setup(tenantid);
+
+				
+				ConfigFinder cfg=new ConfigFinder(getServletContext());
+				try {
+					InputSource cfg_stream=cfg.resolveEntity("-//CSPACE//ROOT","cspace-config.xml");
+					String test = IOUtils.toString(cfg_stream.getByteStream());
+					//servlet_response.sendError(HttpServletResponse.SC_BAD_REQUEST, "cspace-config re-loaded"+test);
+					servlet_response.getWriter().append("cspace-config re-loaded"+test);
+				} catch (SAXException e) {
+					// TODO Auto-generated catch block
+					servlet_response.sendError(HttpServletResponse.SC_BAD_REQUEST, "cspace-config re-loadedfailed");
+				}
+				
+				
+				return;
+			}
+			if(!tenantInit.get(tenantid))
+				setup(tenantid);
+			
+			if(locked_down!=null) {
+				//this ended up with a status 200 hmmmm not great so changed it to return a 400... hopefully that wont break anythign else
+
+				servlet_response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Servlet is locked down in a hard fail because of fatal error: "+locked_down);
+				//servlet_response.getWriter().append("Servlet is locked down in a hard fail because of fatal error: "+locked_down);
+				return;
+			}
+			if(perhapsServeFixedContent(servlet_request,servlet_response))
+				return;
+			// Setup our request object
+			UI web=tenantCSPM.get(tenantid).getUI("web");
+			if(!tenantUmbrella.containsKey(tenantid)){
+				synchronized(getClass()) {
+					if(!tenantUmbrella.containsKey(tenantid)) {
+						tenantUmbrella.put(tenantid, new WebUIUmbrella((WebUI)web));
+					}
+				}
+			}
+			try {
+				ConfigRoot root=tenantCSPM.get(tenantid).getConfigRoot();
+				Spec spec=(Spec)root.getRoot(Spec.SPEC_ROOT);
+				WebUIRequest req=new WebUIRequest(tenantUmbrella.get(tenantid),servlet_request,servlet_response,spec.getAdminData().getCookieLife());
+				if(is_composite(req)) {
+					serve_composite(web,req);
+				} else {
+					web.serviceRequest(req);
+					req.solidify(true);
+				}
+			} catch (UIException e) {
+				throw new BadRequestException("UIException",e);
+			}
+		} catch (BadRequestException x) {
+			servlet_response.sendError(HttpServletResponse.SC_BAD_REQUEST, getStackTrace(x));
+		}
+	}
+	
+	/* We do all this very sequentially rather than in one big loop to avoid the fear of weird races and to fail early on parse errors */
+	protected void serve_composite(UI ui,WebUIRequest req) throws UIException {
+		try {
+			// Extract JSON request payload
+			JSONObject in=req.getJSONBody();
+			// Build composite object for each subrequest
+			Map <String,CompositeWebUIRequestPart> subrequests=new HashMap<String,CompositeWebUIRequestPart>();
+			Iterator<?> ki=in.keys();
+			while(ki.hasNext()) {
+				String key=(String)ki.next();
+				JSONObject value=in.getJSONObject(key);
+				CompositeWebUIRequestPart sub=new CompositeWebUIRequestPart(req,value);
+				subrequests.put(key,sub);
+			}
+			// Build a place for results
+			JSONObject out=new JSONObject();
+			// Execute each composite object
+			for(String key : subrequests.keySet()) {
+				CompositeWebUIRequestPart sub=subrequests.get(key);
+				ui.serviceRequest(sub);
+				JSONObject value=sub.solidify();
+				out.put(key,value);
+			}
+			// Send result
+			req.sendJSONResponse(out);
+			req.setOperationPerformed(req.getRequestedOperation());
+			req.solidify(true);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	protected boolean serveContent(HttpServletResponse servlet_response,InputStream is) throws IOException{
+
+		if(is==null)
+			return false; // Not for us
+
+		IOUtils.copy(is,servlet_response.getOutputStream());
+		return true;
+	}
+	
+	protected boolean serverFixedExternalContent(HttpServletRequest servlet_request, HttpServletResponse servlet_response,ServletContext sc,String path) throws IOException{
+
+        InputStream is=sc.getResourceAsStream(path);
+        return serveContent(servlet_response,is);
+	}
+	
+	protected boolean is_composite(WebUIRequest req) throws UIException {
+		String[] path=req.getPrincipalPath();
+		if(path.length!=1)
+			return false;
+		return "composite".equals(path[1]);
+	}
+	
+	public static String getStackTrace(Throwable aThrowable) {
+		final Writer result = new StringWriter();
+		final PrintWriter printWriter = new PrintWriter(result);
+		aThrowable.printStackTrace(printWriter);
+		return result.toString();
+	}
+	
+	protected boolean perhapsServeFixedContent(HttpServletRequest servlet_request, HttpServletResponse servlet_response) throws ServletException, IOException {
+		String pathinfo=servlet_request.getPathInfo();
+		if(pathinfo.startsWith("/"))
+			pathinfo=pathinfo.substring(1);
+		InputStream is=Thread.currentThread().getContextClassLoader().getResourceAsStream(pathinfo);
+		if(is==null)
+			return false; // Not for us
+		// Serve fixed content
+		IOUtils.copy(is,servlet_response.getOutputStream());
+		return true;
+	}
+}
---
+++ tomcat-main/src/main/java/org/collectionspace/chain/controller/TenantUIServlet.java
@@ -0,0 +1,199 @@
+package org.collectionspace.chain.controller;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.util.List;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Node;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.input.TeeInputStream;
+import org.collectionspace.chain.csp.config.ConfigRoot;
+import org.collectionspace.chain.csp.persistence.services.connection.ConnectionException;
+import org.collectionspace.chain.csp.schema.Instance;
+import org.collectionspace.chain.csp.schema.Record;
+import org.collectionspace.chain.csp.schema.Spec;
+import org.collectionspace.chain.csp.webui.external.UIMapping;
+import org.collectionspace.chain.csp.webui.main.WebUI;
+import org.collectionspace.csp.api.ui.UI;
+
+public class TenantUIServlet extends TenantServlet {
+	private static final Logger log = LoggerFactory
+			.getLogger(TenantUIServlet.class);
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void service(HttpServletRequest servlet_request,
+			HttpServletResponse servlet_response) throws ServletException,
+			IOException {
+
+		try {
+
+			String pathinfo = servlet_request.getPathInfo();
+			String[] pathbits = pathinfo.substring(1).split("/");
+			String tenant = pathbits[0];
+			
+			if (pathinfo.equals("/html") || pathinfo.equals("/html/")) {
+				servlet_response
+						.sendRedirect("/collectionspace/ui/"+tenant+"/index.html");
+			}
+
+			if (pathinfo.equals("/"+tenant) || pathinfo.equals("/"+tenant+"/")) {
+				servlet_response
+						.sendRedirect("/collectionspace/ui/"+tenant+"/index.html");
+			}
+
+			if(!tenantInit.get(tenant))
+				setup(tenant);
+
+			// should we redirect this url or just do the normal stuff
+
+			// Setup our request object
+			UI web = tenantCSPM.get(tenant).getUI("web");
+			WebUI webui = (WebUI) web;
+			UIMapping[] allmappings = webui.getAllMappings();
+
+			ConfigRoot root = tenantCSPM.get(tenant).getConfigRoot();
+			Spec spec = (Spec) root.getRoot(Spec.SPEC_ROOT);
+
+			ServletContext sc = null;
+			sc = getServletContext().getContext("/cspace-ui");
+			if(sc==null){
+				servlet_response.sendError(HttpServletResponse.SC_BAD_REQUEST,"missing servlet context cspace-ui");
+			}
+
+			Boolean doMetaConfig = false;
+			String path = pathinfo;
+			UIMapping validmap = doMapping(pathinfo, allmappings, spec);
+			if (null != validmap) {
+				path = validmap.getFile();
+				if (validmap.hasMetaConfig() && doMetaConfig) {
+					InputStream is = sc.getResourceAsStream(path);
+					
+					SAXReader reader = new SAXReader();
+					reader.setEntityResolver(new NullResolver());
+					Document xmlfile = null;
+
+					ByteArrayOutputStream dump = new ByteArrayOutputStream();
+					xmlfile = reader.read(new TeeInputStream(is, dump));
+				//	log.info(dump.toString("UTF-8"));
+
+					for (String metafield : validmap.getAllMetaConfigs()) {
+						String xPath = "//html/head/" + metafield;
+						List<Node> nodes = xmlfile.selectNodes(xPath);
+
+						if (!nodes.isEmpty()) {
+							String fieldval = validmap.getMetaConfig(metafield)
+									.getValue();
+							nodes.get(0).setText(fieldval);
+						}
+					}
+					InputStream source = documentToStream(xmlfile);
+					serveContent(servlet_response, source);
+
+					is.close();
+					return;
+				}
+			}
+
+			if (serverFixedExternalContent(servlet_request, servlet_response,
+					sc, path)) {
+				return;
+			}
+
+		} catch (BadRequestException x) {
+			servlet_response.sendError(HttpServletResponse.SC_BAD_REQUEST,
+					getStackTrace(x));
+		} catch (ConnectionException e) {
+			servlet_response.sendError(HttpServletResponse.SC_BAD_REQUEST,
+					getStackTrace(e));
+		} catch (DocumentException e) {
+			servlet_response.sendError(HttpServletResponse.SC_BAD_REQUEST,
+					getStackTrace(e));
+		}
+	}
+	/* borrowed from ContectionUtils and tweaked e.g. set newline = true*/
+	static InputStream serializetoXML(Document doc) throws IOException {
+		return new ByteArrayInputStream(serializeToBytes(doc));
+	}
+
+	static byte[] serializeToBytes(Document doc) throws IOException {
+		ByteArrayOutputStream out=new ByteArrayOutputStream();
+		OutputFormat outformat = OutputFormat.createPrettyPrint();
+		outformat.setExpandEmptyElements(true);
+		outformat.setNewlines(true);
+		outformat.setIndent(false);
+		XMLWriter writer = new XMLWriter(out, outformat);
+		writer.write(doc);
+		writer.flush();
+		out.close();
+		return out.toByteArray();
+	}
+	
+	public static InputStream documentToStream(Document in) throws ConnectionException {
+		if(in!=null) {
+			try {
+				return serializetoXML(in);
+			} catch (IOException e) {
+				throw new ConnectionException("Could not connect "+e.getLocalizedMessage(),e);
+			}
+		}
+		return null;
+	}
+
+	private UIMapping doMapping(String pathinfo, UIMapping[] allmappings,
+			Spec spec) {
+		for (UIMapping map : allmappings) {
+
+			if (map.hasUrl()) {
+				if (map.getUrl().equals(pathinfo)) {
+					return map;
+				}
+			} else if (map.hasType()) {
+				for (Record r : spec.getAllRecords()) {
+					if (r.isType(map.getType())) {
+						if(r.isType("authority")){
+							for(Instance ins: r.getAllInstances()){
+								if (pathinfo.equals("/"+ spec.getAdminData().getTenant() +"/" + ins.getUIURL())) {
+									return map;
+								}
+							}
+						}
+						if (pathinfo.equals("/"+spec.getAdminData().getTenant()+"/" + r.getUIURL())) {
+							return map;
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+}
+
+class NullResolver implements EntityResolver {
+	public InputSource resolveEntity(String publicId, String systemId)
+			throws SAXException, IOException {
+		return new InputSource(new StringReader(""));
+	}
+}
