239475ae77fd7eb61a3444f6e183f94077efcd5e
Chris Martin <csm22@cam.ac.uk>
2011-06-14 10:30:20 +0000

    CSPACE-4019

--- tomcat-main/src/test/java/org/collectionspace/chain/csp/persistence/services/vocab/TestOrgThroughWebapp.java
+++ tomcat-main/src/test/java/org/collectionspace/chain/csp/persistence/services/vocab/TestOrgThroughWebapp.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.commons.io.IOUtils;
-import org.collectionspace.chain.controller.ChainServlet;
+import org.collectionspace.chain.controller.TenantServlet;
 import org.collectionspace.chain.csp.persistence.TestBase;
 import org.json.JSONArray;
 import org.json.JSONException;
