8d1aa680e8f025ac745483395f3ef4ebaab59c9f
Dan Sheppard <dan.sheppard@caret.cam.ac.uk>
2011-07-21 15:58:08 +0000

    CSPACE-4071 Configurable XSD generation.

--- tomcat-main/src/main/java/org/collectionspace/chain/controller/CommandLine.java
+++ tomcat-main/src/main/java/org/collectionspace/chain/controller/CommandLine.java
@@ -1,14 +1,19 @@
 package org.collectionspace.chain.controller;
 
+/* Invoke from command line with recordtype, domain, maketype (core/delta) and configfile
+ * 
+ * eg java -jar cspace/conf/cspace.jar collectionobjects collectionspace_core core lifesci-tenant.xml
+ * 
+ */
+
 import org.collectionspace.chain.installation.XsdGeneration;
-import org.collectionspace.csp.api.ui.UIException;
 
 public class CommandLine {
 	public static final void main(String[] args) throws Exception {
-		String recordtype = "collectionobjects";
-		String domain = "collectionspace_core";
-		String maketype = "core";
-		String configfile = "lifesci-tenant.xml";
+		String recordtype = args[0];
+		String domain = args[1];
+		String maketype = args[2];
+		String configfile = args[3];
 		XsdGeneration s = new XsdGeneration(configfile, recordtype, domain, maketype);
 		String xsdschema = s.getFile();
 		System.err.println(xsdschema);
