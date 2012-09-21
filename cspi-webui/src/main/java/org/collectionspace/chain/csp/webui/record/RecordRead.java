e650cb7c214c1d689adef9f9e566dac00313fa3f
Patrick Schmitz <pschmitz@berkeley.edu>
2012-09-21 12:43:09 -0700

    CSPACE-5550 Ditto last change, same case for cataloging.

--- cspi-webui/src/main/java/org/collectionspace/chain/csp/webui/record/RecordRead.java
+++ cspi-webui/src/main/java/org/collectionspace/chain/csp/webui/record/RecordRead.java
@@ -375,10 +375,10 @@ public class RecordRead implements WebMethod {
 	private JSONObject getHierarchy(Storage storage, JSONObject fields) throws JSONException, ExistException, UnimplementedException, UnderlyingStorageException{
 		for(Relationship r: record.getSpec().getAllRelations()){
 			if(r.showSiblings()){
-				JSONObject temp = new JSONObject();
-				temp.put("_primary", true);
+				//JSONObject temp = new JSONObject();
+				//temp.put("_primary", true);
 				JSONArray children = new JSONArray();
-				children.put(temp);
+				//children.put(temp);
 				fields.put(r.getSiblingParent(), children);
 				if(fields.has(r.getID())){
 					//String broadterm = fields.getString(r.getID());
